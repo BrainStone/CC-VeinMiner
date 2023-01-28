4f03c8dde704747d601a29ddb37eededcb57350a
BrainStone <yannick@tedworld.de>
2023-01-28 02:43:54 +0100

    Use built ins

--- src/install.lua
+++ src/install.lua
@@ -3,13 +3,11 @@
 ---
 
 -- Path of this script
-local cur_path = debug.getinfo(1).source
-if cur_path:sub(1,1) == "@" then
-	cur_path = cur_path:sub(2)
-end
-local directory = cur_path:match("(.*/)")
+local cur_path = shell.getRunningProgram()
+local cur_dir = fs.getDir(cur_path)
 
 -- Path to the main.lua file
 local main_path_name = "main.lua"
-local main_path = directory .. main_path_name
+local main_path = cur_dir .. main_path_name
+
 print(main_path)
