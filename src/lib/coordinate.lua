399579881193ef7741e245e547410b5a365d7582
BrainStone <yannick@tedworld.de>
2023-01-29 00:05:05 +0100

    Update docs

--- src/lib/coordinate.lua
+++ src/lib/coordinate.lua
@@ -3,17 +3,17 @@
 ---
 
 --- Coordinate class
--- @field x x coordinate
--- @field y y coordinate
--- @field z z coordinate
+--- @field x x coordinate
+--- @field y y coordinate
+--- @field z z coordinate
 Coordinate = {}
 Coordinate.__index = Coordinate
 
 --- Creates a new Coordinate object
--- @param x x coordinate
--- @param y y coordinate
--- @param z z coordinate
--- @return a new Coordinate object
+--- @param x x coordinate
+--- @param y y coordinate
+--- @param z z coordinate
+--- @return a new Coordinate object
 function Coordinate:new(x, y, z)
 	local coord = {}
 	setmetatable(coord, self)
@@ -24,10 +24,10 @@ function Coordinate:new(x, y, z)
 end
 
 --- Calculates the manhattan distance between two coordinates
--- @param x x coordinate of the other point or a Coordinate object
--- @param y y coordinate of the other point or nil if x is a Coordinate object
--- @param z z coordinate of the other point or nil if x is a Coordinate object
--- @return distance as a number
+--- @param x x coordinate of the other point or a Coordinate object
+--- @param y y coordinate of the other point or nil if x is a Coordinate object
+--- @param z z coordinate of the other point or nil if x is a Coordinate object
+--- @return distance as a number
 function Coordinate:distance(x, y, z)
 	if type(x) == "table" and x.x and x.y and x.z then
 		return self:distance(x.x, x.y, x.z)
@@ -39,11 +39,11 @@ function Coordinate:distance(x, y, z)
 end
 
 --- Returns a new Coordinate object with the offset applied
--- @param dx x offset
--- @param dy y offset
--- @param dz z offset
--- @param or offset which can be a number or a Coordinate object
--- @return new Coordinate object
+--- @param dx x offset
+--- @param dy y offset
+--- @param dz z offset
+--- @param or offset which can be a number or a Coordinate object
+--- @return new Coordinate object
 function Coordinate:offset(dx, dy, dz)
 	if type(dx) == "table" and dx.x and dx.y and dx.z then
 		return self:offset(dx.x, dx.y, dx.z)
@@ -60,15 +60,15 @@ function Coordinate:offset(dx, dy, dz)
 end
 
 --- Overloads the + operator for Coordinate objects
--- @param other Coordinate object or a number to add
--- @return new Coordinate object
+--- @param other Coordinate object or a number to add
+--- @return new Coordinate object
 function Coordinate:__add(other)
 	return self:offset(other)
 end
 
 --- Overloads the - operator for Coordinate objects
--- @param other Coordinate object to subtract
--- @return distance as a number
+--- @param other Coordinate object to subtract
+--- @return distance as a number
 function Coordinate:__sub(other)
 	return self:distance(other)
 end
--- src/util/include.lua
+++ src/util/include.lua
@@ -1,6 +1,11 @@
 ---
 --- Inclusion helper
 ---
+--- This script is a helper for including other files in a program.
+--- It provides the following functions:
+--- - `loadLib(lib)`: loads a library from the `lib` directory.
+--- - `runFile(file, ...)`: runs a file from the source directory.
+---
 --- Include with this snippet:
 --- -- Path of this script
 --- local cur_path = shell.getRunningProgram()
@@ -16,11 +21,14 @@ repo_dir = fs.getDir(src_dir)
 lib_dir = fs.combine(src_dir, "lib")
 
 --- Load a library
+---- @param lib the library to load
 function loadLib(lib)
 	_G[lib] = require("lib." .. lib)
 end
 
 --- Run a file
+--- @param file the file to run
+--- @param ... additional arguments to pass to the file
 function runFile(file, ...)
 	local f = loadfile(fs.combine(src_dir, file))
 	setfenv(f, getfenv())
