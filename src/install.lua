4074de5a994c78ad4fd016c618a312be1c30d932
BrainStone <yannick@tedworld.de>
2023-01-28 03:11:03 +0100

    Use startup dir

--- src/install.lua
+++ src/install.lua
@@ -11,8 +11,19 @@ local base_dir = fs.getDir(cur_dir)
 local main_path_name = "main.lua"
 local main_path = fs.combine(cur_dir, main_path_name)
 
+-- Create startup dir and migrate potentially preexisting startup scripts
+if os.exists("startup") then
+	if not os.isDir("startup") then
+		os.rename("startup", "startup.old.temp")
+		os.makeDir("startup")
+		os.rename("startup.old.temp", "startup/01-orig_startup")
+	end
+else
+	os.makeDir("startup")
+end
+
 -- Write startup file
-local h = fs.open("startup", "w")
+local h = fs.open("startup/50-vein_miner", "w")
 
 h.writeLine("-- Update repo first")
 h.writeLine("shell.run(\"github\", \"clone\", \"BrainStone/CC-VeinMiner\", \"" .. base_dir .. "\")")
