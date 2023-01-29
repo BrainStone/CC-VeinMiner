c8fb2031e425ea6483a96088ad9e5791f21006bb
BrainStone <yannick@tedworld.de>
2023-01-29 23:29:25 +0100

    Mark blocks with color

--- src/main.lua
+++ src/main.lua
@@ -24,7 +24,7 @@ function spiral_coordinates(n)
 		x = level2
 		z = ((level2 * 2) - level_offset) * direction
 	elseif level_offset < (level2 * 4) then
-		x = ((level2 * 4) - level_offset)
+		x = (level2 * 4) - level_offset
 		z = -level2 * direction
 	else
 		x = 0
@@ -34,9 +34,28 @@ function spiral_coordinates(n)
 	return coordinate:new(x, 0, z)
 end
 
-for n = 0, 100 do
+local place_slot
+
+for n = 0, 500 do
 	movement.moveToPosition(spiral_coordinates(n) + movement.start_position.coordinate)
 	turtle.digDown()
+
+	place_slot = 0
+
+	if n % 3 == 0 then
+		place_slot = place_slot + 3
+	end
+	if n % 4 == 0 then
+		place_slot = place_slot + 4
+	end
+	if n % 5 == 0 then
+		place_slot = place_slot + 5
+	end
+
+	if place_slot ~= 0 then
+		turtle.select(place_slot)
+		turtle.placeDown()
+	end
 end
 
 movement.moveHome()
